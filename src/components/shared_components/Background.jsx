b270c9222d9dd5df4717ba3e63db1e1a54d337bc
miknhoj <kimjohan86@gmail.com>
2018-11-19 16:38:40 -0500

    override parallax default line-height

--- src/components/shared_components/Background.jsx
+++ src/components/shared_components/Background.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Parallax } from 'react-parallax';
 import styled from 'styled-components'
 
+const StyledWrapper = styled.div`
+  text-align: center;
+`
+
 const StyledImage = styled.div`
   height: 500px;
 `
@@ -14,18 +18,27 @@ const StyledText = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%,-50%);
+  line-height: 1;
+
+
+  /* @media(min-width: 1000px) {
+      font-size: 40px;
+  }
+  @media(max-width: 600px) {
+      font-size: 50px;
+      line-height: 1;
+  } */
 `
 
 const image = "https://images.unsplash.com/photo-1482376297902-a54c222cec2b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b054282b3f7a02bdca851ae7f09802cc&auto=format&fit=crop&w=1650&q=80"
 
 const Background = () => (
-  <div>
-    {/* -----dynamic blur-----*/}
+  <StyledWrapper>
     <Parallax bgImage={image} >
       <StyledImage >
         <StyledText>Hi. I'm John.</StyledText>
       </StyledImage >
     </Parallax>
-  </div>
+  </StyledWrapper>
 );
 export default Background;
