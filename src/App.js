923385dd6f112afa57561ac9301ba1a7b54fbea0
miknhoj <kimjohan86@gmail.com>
2018-11-29 19:50:21 -0500

    add project cards

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects'
+// import About from './components/About';
+// import Projects from './components/Projects'
 import NavBar from './components/NavBar'
 
 class App extends Component {
@@ -17,8 +17,8 @@ class App extends Component {
 
           <Switch>
             <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-            <Route exact path='/projects' component={Projects} />
+            {/* <Route exact path='/about' component={About} />
+            <Route exact path='/projects' component={Projects} /> */}
 
           </Switch>
         </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,68 @@
 import React, { Component } from 'react'
+import { Card, Icon, Image } from 'semantic-ui-react'
 
 export default class Projects extends Component {
   render() {
     return (
       <div>
-        Projects
+        <Card.Group>
+          <Card>            
+            <Image src='/images/avatar/large/daniel.jpg' onClick={()=> window.open("https://awesome-gates-834535.netlify.com/", "_blank")}/>
+            <Card.Content>
+              <Card.Header>Project 1</Card.Header>
+              <Card.Meta>Joined in 2016</Card.Meta>
+              <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user' />
+                10 Friends
+      </a>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Image src='/images/avatar/large/daniel.jpg' onClick={()=> window.open("https://infinite-crag-33348.herokuapp.com/", "_blank")}/>
+            <Card.Content>
+              <Card.Header>Project 2</Card.Header>
+              <Card.Meta>Joined in 2016</Card.Meta>
+              <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user' />
+                10 Friends
+      </a>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Image src='/images/avatar/large/daniel.jpg' onClick={()=> window.open("https://cigar-log.herokuapp.com/", "_blank")}/>
+            <Card.Content>
+              <Card.Header>Project 3</Card.Header>
+              <Card.Meta>Joined in 2016</Card.Meta>
+              <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user' />
+                10 Friends
+      </a>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Image src='/images/avatar/large/daniel.jpg' onClick={()=> window.open("https://costanza-go.herokuapp.com/", "_blank")}/>
+            <Card.Content>
+              <Card.Header>Project 4</Card.Header>
+              <Card.Meta>Joined in 2016</Card.Meta>
+              <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user' />
+                10 Friends
+      </a>
+            </Card.Content>
+          </Card>Í
+        </Card.Group>
       </div>
     )
   }
