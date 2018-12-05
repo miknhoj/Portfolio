cd85087c6d43f0895d4218167b587c17cfa4d3d3
miknhoj <kimjohan86@gmail.com>
2018-12-05 16:01:53 -0500

    add skills icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ import { Segment, Container, Grid, List, Header, Icon, Modal, Image } from 'sema
 
 export default class Footer extends Component {
   render() {
+
     return (
       <Segment inverted vertical style={{ padding: '5em 0em' }}>
         <Container>
@@ -13,9 +14,22 @@ export default class Footer extends Component {
                 <Header as='h3' inverted>
                   Skills
               </Header>
-                <p>
-                  Extra space for a call to action inside the footer that could help re-engage users.
-              </p>
+                <List horizontal >
+                  <List.Item><i className="devicon-html5-plain-wordmark"></i></List.Item>
+                  <List.Item><i className="devicon-css3-plain-wordmark"></i></List.Item>
+                  <List.Item><i className="devicon-javascript-plain"></i></List.Item>
+                  <List.Item><i className="devicon-nodejs-plain-wordmark"></i></List.Item>
+                  <List.Item><i className="devicon-react-original-wordmark"></i>></List.Item>
+                  <List.Item><i className="devicon-mongodb-plain-wordmark"></i></List.Item>
+                  <List.Item><i className="devicon-postgresql-plain-wordmark"></i></List.Item>
+                  <List.Item><i className="devicon-ruby-plain"></i></List.Item>
+                  <List.Item><i className="devicon-rails-plain-wordmark"></i></List.Item>
+                  <List.Item><i className="devicon-bootstrap-plain-wordmark"></i></List.Item>
+                  <List.Item><i className="devicon-express-original-wordmark"></i></List.Item>
+                  <List.Item><i className="devicon-git-plain-wordmark"></i></List.Item>
+                  <List.Item><i className="devicon-heroku-original-wordmark"></i></List.Item>
+                  <List.Item><i className="devicon-jquery-plain-wordmark"></i></List.Item>
+                </List>
               </Grid.Column>
 
               <Grid.Column width={3}>
@@ -28,7 +42,7 @@ export default class Footer extends Component {
                   <Modal trigger={<List.Item as='a'><Icon name='file alternate outline' />Resume</List.Item>} closeIcon>
                     <Header icon='file alternate outline' content='Resume' />
                     <Modal.Content image>
-                    <Image wrapped size='fullscreen' src = './images/Resume_John_Kim.jpg' alt='resume'/>
+                      <Image wrapped size='huge' src='./images/Resume_John_Kim.jpg' alt='resume' />
                     </Modal.Content>
                   </Modal>
                   {/* <List.Item as={Link} to='/resume'><Icon name='file alternate outline' />Resume</List.Item> */}
